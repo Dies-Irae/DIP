080007e08b008ea7d026913a2329f87985bea207
Dies-Irae <30491121+Dies-Irae@users.noreply.github.com>
2020-10-26 11:46:04 +0800

    2.fin

--- coins.py
+++ coins.py
@@ -3,6 +3,7 @@ import numpy as np
 coins = cv2.imread("coins.jpg")
 coinsGray = cv2.cvtColor(coins, cv2.COLOR_BGR2GRAY)
 ret, coinsBin = cv2.threshold(coins, 95, 255, cv2.THRESH_BINARY)
+
 kern = np.ones((5, 5), np.uint8)
 coinsEroded = cv2.erode(coinsBin, kernel=kern, iterations=5)
 coinsEroded = cv2.cvtColor(coinsEroded, cv2.COLOR_BGR2GRAY)
@@ -10,7 +11,7 @@ contours, hierarchy = cv2.findContours(coinsEroded, cv2.RETR_TREE, cv2.CHAIN_APP
 coinsEroded = cv2.cvtColor(coinsEroded, cv2.COLOR_GRAY2BGR)
 coinsDrawn = coinsEroded.copy()
 coinsDrawn = cv2.drawContours(coinsDrawn, contours, -1, (0,255,0), 3)
-res = np.hstack((coins,  coinsEroded, coinsDrawn))
+res = np.hstack((coins,coinsBin, coinsEroded, coinsDrawn))
 cv2.imshow('1', res)
 cv2.waitKey(0)
 cv2.imwrite('res.jpg', res)
